6db50300e1359b9b62e37e19a9134dfb9fdc3d6e
Tord Standnes <tord.standnes@gmail.com>
2020-07-20 14:16:50 +0200

    webflow convertion

--- playly_prototype/src/index.js
+++ playly_prototype/src/index.js
@@ -1,25 +1,15 @@
-import { Provider } from 'react-redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as SWRTC from '@andyet/simplewebrtc';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react' 
+import ReactDOM from 'react-dom'
 
-const store = SWRTC.createStore();
+class App extends Component {
+ render() {
+   return (
+     <div>This is a React component inside of Webflow!</div>
+  )
+}
+}
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+React.createElement(App, {}, null),
+document.getElementById('react-target')
 );
-
-export default store;
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
