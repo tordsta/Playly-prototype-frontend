9008f8d139beb4dbc658c483d68f98d3a5b01a54
Tord Standnes <tord.standnes@gmail.com>
2020-10-09 19:26:59 +0200

    wss upgrade

--- src/index.js
+++ src/index.js
@@ -11,34 +11,16 @@ import RTCMesh from './ReactRTC/RTCMesh';
 require("./ReactRTC/index.css")
 
 //const url = "ws://localhost:3000"
-//const url = "ws://playly-server-alb-732449255.us-east-2.elb.amazonaws.com";
+const url = "wss://playlyserver.net";
 
 class App extends Component {
-  //ws = new WebSocket(url)
-
-  //This is currently only for testing pourposes
-  componentDidMount() {
-    //this.ws.onopen = () => {
-      // on connecting, do nothing but log it to the console
-    //  console.log('connected')
-    //}
-
-    //this.ws.onclose = () => {
-    //  console.log('disconnected')
-      // fix this - automatically try to reconnect on connection loss
-      //this.setState({
-      //  ws: new WebSocket(URL),
-      //})
-    //}
-  }
 
   render() {
     return (
       <div>        
-        This is a ReactRTC component imported from outside webflow<br/>
 
         <ErrorBoundary>
-          <RTCMesh URL="ws://localhost:3000"/>
+          <RTCMesh URL={url}/>
         </ErrorBoundary>        
                 
       </div>     
